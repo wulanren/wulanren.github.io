db2ae2300e3d3bbfbb18a2665d0db18322bff4f1
wulanren <you@example.com>
2020-03-11 18:28:24 +0800

    上传
    上传

---
+++ js/Mercedes遮罩.js
@@ -0,0 +1,115 @@
+// 触发遮罩层按钮
+var opens01 = document.querySelector("#tcydgd01");
+var opens02 = document.querySelector("#tcydgd02");
+var opens03 = document.querySelector("#tcydgd03");
+var opens04 = document.querySelector("#tcydgd04");
+var opens05 = document.querySelector("#tcydgd05");
+var opens06 = document.querySelector("#tcydgd06");
+var opens07 = document.querySelector("#tcydgd07");
+var opens08 = document.querySelector("#tcydgd08");
+
+
+//关闭遮罩层按钮
+var closes01 = document.querySelector("#guanbi01");
+var closes02 = document.querySelector("#guanbi02");
+var closes03 = document.querySelector("#guanbi03");
+var closes04 = document.querySelector("#guanbi04");
+var closes05 = document.querySelector("#guanbi05");
+var closes06 = document.querySelector("#guanbi06");
+var closes07 = document.querySelector("#guanbi07");
+var closes08 = document.querySelector("#guanbi08");
+
+
+//弹出的页面内容
+var cover01 = document.querySelector("#cover01");
+var cover02 = document.querySelector("#cover02");
+var cover03 = document.querySelector("#cover03");
+var cover04 = document.querySelector("#cover04");
+var cover05 = document.querySelector("#cover05");
+var cover06 = document.querySelector("#cover06");
+var cover07 = document.querySelector("#cover07");
+var cover08 = document.querySelector("#cover08");
+
+// 内容框
+var modal01 = document.querySelector("#tcnr01");
+var modal02 = document.querySelector("#tcnr02");
+var modal03 = document.querySelector("#tcnr03");
+var modal04 = document.querySelector("#tcnr04");
+var modal05 = document.querySelector("#tcnr05");
+var modal06 = document.querySelector("#tcnr06");
+var modal07 = document.querySelector("#tcnr07");
+var modal08 = document.querySelector("#tcnr08");
+
+//1
+opens01.onclick = function() {
+    cover01.style.display = "block"; //显示遮罩层
+    modal01.style.display = "block"; //显示弹出层
+};
+closes01.onclick = function() {
+    cover01.style.display = "none"; //隐藏遮罩层
+    modal01.style.display = "none"; //隐藏弹出层
+};
+//2
+opens02.onclick = function() {
+    cover02.style.display = "block"; //显示遮罩层
+    modal02.style.display = "block"; //显示弹出层
+};
+closes02.onclick = function() {
+    cover02.style.display = "none"; //隐藏遮罩层
+    modal02.style.display = "none"; //隐藏弹出层
+};
+//3
+opens03.onclick = function() {
+    cover03.style.display = "block"; //显示遮罩层
+    modal03.style.display = "block"; //显示弹出层
+};
+closes03.onclick = function() {
+    cover03.style.display = "none"; //隐藏遮罩层
+    modal03.style.display = "none"; //隐藏弹出层
+};
+//4
+opens04.onclick = function() {
+    cover04.style.display = "block"; //显示遮罩层
+    modal04.style.display = "block"; //显示弹出层
+};
+closes04.onclick = function() {
+    cover04.style.display = "none"; //隐藏遮罩层
+    modal04.style.display = "none"; //隐藏弹出层
+};
+//5
+opens05.onclick = function() {
+    cover05.style.display = "block"; //显示遮罩层
+    modal05.style.display = "block"; //显示弹出层
+};
+closes05.onclick = function() {
+    cover05.style.display = "none"; //隐藏遮罩层
+    modal05.style.display = "none"; //隐藏弹出层
+};
+//6
+opens06.onclick = function() {
+    cover06.style.display = "block"; //显示遮罩层
+    modal06.style.display = "block"; //显示弹出层
+};
+closes06.onclick = function() {
+    cover06.style.display = "none"; //隐藏遮罩层
+    modal06.style.display = "none"; //隐藏弹出层
+};
+
+//7
+opens07.onclick = function() {
+    cover07.style.display = "block"; //显示遮罩层
+    modal07.style.display = "block"; //显示弹出层
+};
+closes07.onclick = function() {
+    cover07.style.display = "none"; //隐藏遮罩层
+    modal07.style.display = "none"; //隐藏弹出层
+};
+//8
+opens08.onclick = function() {
+    cover08.style.display = "block"; //显示遮罩层
+    modal08.style.display = "block"; //显示弹出层
+};
+closes08.onclick = function() {
+    cover08.style.display = "none"; //隐藏遮罩层
+    modal08.style.display = "none"; //隐藏弹出层
+};
